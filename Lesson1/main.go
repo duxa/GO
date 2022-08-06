61cda86167988fae307e6a003b8f843ad039eebe
duxa <duxa321@gmail.com>
2022-08-06 19:20:13 +0300

    update

--- Lesson1/main.go
+++ Lesson1/main.go
@@ -1,1 +1,5 @@
-package Lesson1
+package main // говорим, что файл принадлежит пакету main
+// func -- объявление функции
+func main() { // func main() -- точка входа
+	println("Hello, world !") //
+}
